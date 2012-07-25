7c7aa1183de354e984abe5800a3103c79e1ec0f6
lance bradley <l@ncebradley.org>
2012-07-24 19:08:23 -0700

    - add logging - make content-type application/json - use sweet new require/refer stuff

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-json "0.5.1"]
-                 [ring/ring-servlet "1.1.1"]]
+                 [ring/ring-servlet "1.1.1"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [log4j/log4j "1.2.17"]]
   :plugins [[lein-ring "0.7.1"]]
   :ring {:handler error-servlet.core/handler}
   :main error-servlet.core)
--- src/error_servlet/core.clj
+++ src/error_servlet/core.clj
@@ -1,12 +1,14 @@
 (ns error-servlet.core
-  (:require [clj-json.core :as json])
-  (:use [ring.util.servlet :only [defservice]])
+  (:require [clj-json.core :refer [generate-string]]
+            [clojure.tools.logging :refer [error]]
+            [ring.util.servlet :refer [defservice]])
   (:gen-class :extends javax.servlet.http.HttpServlet))
 
 (defn handler [req]
+  (error "Context Missing:" (:uri req))
   {:status  500
-   :headers {"Content-Type" "text/html"}
-   :body    (json/generate-string {:message "no context deployed to fulfill this request"
-                                   :uri (:uri req)})})
+   :headers {"Content-Type" "application/json"}
+   :body    (generate-string {:message "no context deployed to fulfill this request"
+                              :uri (:uri req)})})
 
 (defservice handler)
